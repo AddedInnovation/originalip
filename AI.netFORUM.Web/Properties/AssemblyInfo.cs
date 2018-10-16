15a809d6ba35a3d3ffd6b4eca1acb3d9cbc1ae0f
Mark Whisler <mwhisler@addedinnovation.com>
2018-10-16 11:04:10 -0500

    Renamed the solution.

--- AI.netFORUM.Web/Properties/AssemblyInfo.cs
+++ AI.netFORUM.Web/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AI.netFORUM.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("netFORUM utilities for iweb, eweb and xweb")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Added Innovation")]
 [assembly: AssemblyProduct("AI.netFORUM.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
